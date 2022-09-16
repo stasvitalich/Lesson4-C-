1725c0d8b6188552fcbc1378c081b1a0904c9e83
Stanislav <stasvitalich1994@gmail.com>
2022-09-16 20:57:49 +0700

    Folder Seminar1 has been created

--- Lesson5Fibonacci/Program.cs
+++ Lesson5Fibonacci/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Вычисление первых 40 чисел Финбоначчи
+
+double Fibonacci (int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for(int i = 1; i < 40; i = i + 1)
+{
+    Console.WriteLine(Fibonacci(i));
+}
