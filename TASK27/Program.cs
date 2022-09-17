13594deb6e7568fe2c63d79cf49080305f469e1d
Stanislav <stasvitalich1994@gmail.com>
2022-09-17 14:27:03 +0700

    Method has been created

--- TASK27/Program.cs
+++ TASK27/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//ЗАДАЧА 27
+//Напишите программу, которая принимает на вход число
+//и выдаёт сумму цифр в числе.
+//Пример:
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+//Напишем метод, который принимает на вход число
+//и возвращает число
+
+
+
+int GetSum (int number)
+{   
+    int sum = 0;
+    for (int i = 0; i <= 10; i = number)
+    {
+        number = number/10;
+        
+    }
+    return number;  
+}
+ 
+
+//Инициируем ввод переменной
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = GetSum(number);
+Console.WriteLine(result);
