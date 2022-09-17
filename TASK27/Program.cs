175b1bb0245040772651bcf79e486063102a75f8
Stanislav <stasvitalich1994@gmail.com>
2022-09-17 15:01:55 +0700

    TASK27 HAS BEEN RESOLVED

--- TASK27/Program.cs
+++ TASK27/Program.cs
@@ -8,24 +8,33 @@
 
 //Напишем метод, который принимает на вход число
 //и возвращает число
-
-
-
 int GetSum (int number)
 {   
     int sum = 0;
-    for (int i = 0; i <= 10; i = number)
+    int total = 0;
+    for (int i = 1; i <= 10; i = i+1)
     {
+        sum = number % 10;
         number = number/10;
-        
+        total = total + sum;
     }
-    return number;  
+    return total;  
 }
  
-
 //Инициируем ввод переменной
 Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
 
-int result = GetSum(number);
-Console.WriteLine(result);
+try 
+{
+    int number = int.Parse(Console.ReadLine());
+
+    //Через новую переменную распечатаем метод:
+    int result = Math.Abs(GetSum(number));
+    Console.WriteLine(result);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: вы ввели не число, а строку. Попробуйте снова. Error: {ex.Message}");
+}
+    
