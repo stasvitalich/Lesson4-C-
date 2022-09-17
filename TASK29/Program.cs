fbc9345c8e1059fb08ab435ce200812d06df631b
Stanislav <stasvitalich1994@gmail.com>
2022-09-17 17:05:04 +0700

    TASK29 HAS BEEN RESOLVED

--- TASK29/Program.cs
+++ TASK29/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//ЗАДАЧА 29
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//Элементы массива вводятся пользователем.
+//Пример:
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+//Строго задаём массив из 8 элементов: 
+int [] NewArray = new int [8];
+
+//Через цикл for инициализируем ввод элементов массива 
+//в пределах заданной длины:
+for(int i = 0; i < NewArray.Length; i = i + 1)
+{
+    Console.WriteLine($"Введите элемент массива с индексом {i}");
+    NewArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.Write("Вывод массива: [");
+for (int i = 0; i < NewArray.Length; i = i + 1)
+{   
+        if (i < 7)
+        {
+            Console.Write($"{NewArray[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{NewArray[i]}");
+        }   
+}
+Console.Write("]");
