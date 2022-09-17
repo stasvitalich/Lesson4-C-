4f43080d4637833a609b7634cf4b7e505288fdc2
Stanislav <stasvitalich1994@gmail.com>
2022-09-17 13:02:21 +0700

    TASK25 HAS BEEN RESOLVED THROUGH THE CYCLE

--- TASK25(CYCLE)/Program.cs
+++ TASK25(CYCLE)/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//ЗАДАЧА 25 (решение через цикл)
+
+//Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B. Решить задачу с использованием методов.
+//Пример:
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+//Метод, который принимает на вход 2 числа и возвращает 1 число:
+double GetPow (int a, int b)
+{
+    int pow = 1;
+    for (int i = 1; i <= b; i = i + 1)
+    {
+        pow = pow * a;
+    }
+    return pow;
+}
+
+//Инициализируем ввод переменных:
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+//Выводим метод на печать через новую переменную:
+double result = GetPow(a, b);
+Console.WriteLine(result);
